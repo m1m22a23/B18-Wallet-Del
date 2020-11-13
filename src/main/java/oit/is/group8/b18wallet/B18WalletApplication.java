e3395612d07756e37ae4aeccbb4f36a42c7d3f9c
e1b18075 <e1b18075@oit.ac.jp>
2020-11-13 12:59:15 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/group8/b18wallet/B18WalletApplication.java
@@ -0,0 +1,13 @@
+package oit.is.group8.b18wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B18WalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B18WalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/group8/b18wallet/B18WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.group8.b18wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B18WalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
