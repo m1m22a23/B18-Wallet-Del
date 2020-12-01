002a19e56dce91ac9324d8bb7e6749ca49b7fdda
e1b18012 <e1b18012.oit.ac.jp>
2020-12-01 16:33:38 +0900

    income.htmlでの入力情報をDBのINCOMEにinsertする

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,7 +6,8 @@ CREATE TABLE income (
     id IDENTITY,
     user_id CHAR NOT NULL REFERENCES users(user_id),
     date CHAR NOT NULL,
-    money INT NOT NULL
+    money INT NOT NULL,
+    memo CHAR
 );
 CREATE TABLE spend (
     id IDENTITY,
