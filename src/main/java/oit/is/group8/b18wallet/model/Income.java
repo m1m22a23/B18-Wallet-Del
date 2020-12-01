f2aca5c4abebd49bf3c6f7c7f4d50aee93db232e
e1b18012 <e1b18012.oit.ac.jp>
2020-12-01 14:41:22 +0900

    データベース内容変更

--- src/main/java/oit/is/group8/b18wallet/model/Income.java
+++ src/main/java/oit/is/group8/b18wallet/model/Income.java
@@ -2,17 +2,17 @@ package oit.is.group8.b18wallet.model;
 
 public class Income {
   int id;
-  int user_id;
+  String user_id;
   String date;
   int money;
 
   // Thymeleafでフィールドを扱うためにはgetter/setterが必ず必要
   // vscodeのソースコード右クリック->ソースアクションでsetter/getterを簡単に追加できる
-  public int getUser_id() {
+  public String getUser_id() {
     return user_id;
   }
 
-  public void setUser(int user_id) {
+  public void setUser(String user_id) {
     this.user_id = user_id;
   }
 
