51cfb6e8ba27e8b6eaf061d9b377972e6fed586e
e1b18090 <e1b18090@st.oit.ac.jp>
2020-12-04 11:25:37 +0900

    spend.htmlでの入力情報をDBのSPENDにinsertする

--- src/main/java/oit/is/group8/b18wallet/controller/SpendController.java
+++ src/main/java/oit/is/group8/b18wallet/controller/SpendController.java
@@ -9,10 +9,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
+import java.security.Principal;
+import oit.is.group8.b18wallet.model.Spend;
+import oit.is.group8.b18wallet.model.SpendMapper;
+import org.springframework.transaction.annotation.Transactional;
 
 @Controller
 @RequestMapping("/spend")
 public class SpendController {
+
+  @Autowired
+  SpendMapper spendMapper;
 
   /**
    * sample21というGETリクエストがあったら，sample21()を呼び出して，sample21.htmlを返すメソッド
@@ -22,6 +29,20 @@ public class SpendController {
 
   @GetMapping("step1")
   public String spend() {
+    return "spend.html";
+  }
+
+  @PostMapping("step2")
+  @Transactional
+  public String spend(@RequestParam String Date, @RequestParam Integer Money, @RequestParam String Memo,
+      Principal prin) {
+    Spend spend1 = new Spend();
+    String loginUser = prin.getName();
+    spend1.setUser(loginUser);
+    spend1.setDate(Date);
+    spend1.setMoney(Money);
+    spend1.setMemo(Memo);
+    spendMapper.insertSpend(spend1);
     return "spend.html";
   }
 }
