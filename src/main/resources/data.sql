d26fd9a6d91dedc666eb0f6e507b3e79b7954049
e1b18012 <e1b18012.oit.ac.jp>
2020-12-11 12:00:42 +0900

    編集フォームのDBへのデータ送信

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,7 @@
 INSERT INTO users (user_id,user) VALUES ('user1','ほんだ');
 INSERT INTO users (user_id,user) VALUES ('user2','いがき');
 INSERT INTO income (user_id,date,money,memo) VALUES ('user1','2000-11-17',100000,'メモでーす');
-INSERT INTO income (user_id,date,money) VALUES ('user2','2000年11月20日',500000);
-INSERT INTO spend (user_id,date,money) VALUES ('user1','2001年11月20日',1000000);
-INSERT INTO spend (user_id,date,money) VALUES ('user2','2001年11月17日',1000000);
+INSERT INTO income (user_id,date,money,memo) VALUES ('user2','2000-11-20',500000,'');
+INSERT INTO spend (user_id,date,money,memo) VALUES ('user1','2001-11-20',1000000,'');
+INSERT INTO spend (user_id,date,money,memo) VALUES ('user2','2001-11-17',1000000,'');
+INSERT INTO spend (user_id,date,money,memo) VALUES ('user2','2001-11-18',1000000,'memo');
